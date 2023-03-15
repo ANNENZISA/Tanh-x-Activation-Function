d72a0e8583194763f3f275a00f4d1c5f6caefe71
Anne Nzisa <95309931+ANNENZISA@users.noreply.github.com>
2023-03-16 02:06:55 +0300

    Create Tanh(x) Activation Function.py

---
+++ Tanh(x) Activation Function.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10, 10, 100)
+y = np.tanh(x)
+
+plt.plot(x, y)
+plt.title('Tanh(x) Activation Function')
+plt.xlabel('Input')
+plt.ylabel('Output')
+plt.show()
